a79c6adfed2c31860ea4ae01cdf7ba47ca330ec5
Prince Takyi <princeitakyi@gmail.com>
2022-12-08 15:15:06 -0800

    prints a specified string using printf and som requirements

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char word[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s", word);
+return (0);
+}
