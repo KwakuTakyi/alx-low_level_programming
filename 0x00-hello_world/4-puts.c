0bfe02ee0b4cf450a350eae8a69d90fbeda70e58
Prince Takyi <princeitakyi@gmail.com>
2022-12-08 15:06:30 -0800

    prints a specified string using puts command only.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char Say[] = "Programming is like building a multilingual puzzle";
+puts(Say); /*writes the string to stdout*/
+return (0);
+}
