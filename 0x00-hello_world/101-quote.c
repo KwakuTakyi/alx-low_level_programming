c4f4322f3e6f892d21e36608ef49dba190f6273f
Prince Takyi <princeitakyi@gmail.com>
2022-12-10 22:16:13 -0800

    prints an exact statement to standard error, not using printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" -
+	       /	Dora Korpar, 2015-10-19\n");
 return (1);
 }
