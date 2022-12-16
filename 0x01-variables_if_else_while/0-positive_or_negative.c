e17212a55aa8c846059970633e2932b0ebbd482a
Prince Takyi <princeitakyi@gmail.com>
2022-12-16 06:17:16 -0800

    assigns a random number to the variable and then checks if it's positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n, i;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{	
+printf("%li is positive\n", (long int)n);
+}
+else if (n < 0) 
+{
+printf("%li is negative\n", (long int)n);
+}
+else if (n == 0)
+{	
+printf("%li is zero\n", (long int)n);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+describes the function of each file
