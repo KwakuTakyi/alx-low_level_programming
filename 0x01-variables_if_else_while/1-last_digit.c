5b0067d6268c719d3d50599b2454af26003af059
Prince Takyi <princeitakyi@gmail.com>
2022-12-16 06:54:28 -0800

    assigns a random number to the variable and then checks if it's positive or negative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,10 +18,10 @@ n = rand() - RAND_MAX / 2;
 i = n % 10;
 
 if (i > 5)
-	printf("Last digit of %d is %d and is greater than 5", n, i);
+	printf("Last digit of %d is %d and is greater than 5\n", n, i);
 else if (i == 0)
-	printf("Last digit of  %d is %d and is 0", n, i);
+	printf("Last digit of  %d is %d and is 0\n", n, i);
 else if ((i < 6) && (i != 0))
-	printf("Last digit of %d is %d and is less than 6 and not 0", n, i);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
 return (0);
 }
