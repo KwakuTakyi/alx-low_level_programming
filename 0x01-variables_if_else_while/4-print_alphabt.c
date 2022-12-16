bcad1fac6615808c65fede9fe1ec771905ff311d
Prince Takyi <princeitakyi@gmail.com>
2022-12-16 08:17:35 -0800

    prints lowercase alphabets and excludes some identified alphabets.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main-Entry
+ *
+ *Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 'q' || i == 'e')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
