ae0f94698abcfede850ea867852d503b27ee4e82
Prince Takyi <princeitakyi@gmail.com>
2022-12-16 17:31:21 -0800

    prints the lowercase alphabet in reverse using only putchar.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main-Entry
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+
+}
+putchar('\n');
+return (0);
+}
