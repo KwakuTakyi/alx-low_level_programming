1e7a108d674a6c9ebc959ee0187cd40b884bfd58
Prince Takyi <princeitakyi@gmail.com>
2022-12-17 04:41:01 -0800

    prints all possible different combinations of two digits. they do not repeat.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,18 +12,15 @@ int i, j;
 
 for (i = '0'; i <= '9'; i++)
 {
-	
 for (j = i + 1; j <= '9'; j++)
 {
-	putchar(i);
-	putchar(j);
-	
-	if (i == '8' && j == '9')
-		continue;
-	
-	putchar(',');
-	putchar(' ');
+putchar(i);
+putchar(j);
 
+if (i == '8' && j == '9')
+	continue;
+putchar(',');
+putchar(' ');
 }
 }
 putchar('\n');
